b2c2a3d699d0159e930036852ee8df5ac8112eaa
Nexxkinn <2881204-Nexxkinn@users.noreply.gitlab.com>
2020-07-21 16:54:23 +0700

    debug pipeline

--- pipeline/tests.ts
+++ pipeline/tests.ts
@@ -78,6 +78,7 @@ await writeJson('test/.vercel/project.json', {
 
 // TODO(lucacasonato): reenable test on macOS
 if (Deno.build.os === 'linux') {
+  console.log(Deno.env.get('NOW_TOKEN'));
   Deno.test({
     name: 'run on now dev',
     async fn() {
